0055cf383b964e68ebe97f5318ce7446d387dc20
Thomas Peikert <t.s.peikert@gmail.com>
2014-11-29 12:28:52 +0100

    extended documentation, added byte visualization

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ github_lang_stats
 
 get language statistics for a github user
 
+#visualize:
 visualizes a user's used languages on github
 ![Github languages](https://github.com/TPei/github_lang_stats/blob/master/img/github_tpei.png)
--- lang_dict.py
+++ lang_dict.py
@@ -1,14 +1,20 @@
 __author__ = 'TPei'
 from github3 import login, GitHub
-import operator
 
-def get_language_dict(user):
+
+def get_language_bytes(user):
+    """
+    count all language usage of a github user
+    :param user: user to get language counts for
+    :return: language: byte dictionary, total bytecount
+    """
     # instead of this, just enter your credentials in variables
     from handle_credentials import username
     from handle_credentials import pw
 
     g = login(username, password=pw)
 
+    # get user to /stalk
     user = g.user(user)
 
     # put all user repos in list
@@ -16,11 +22,14 @@ def get_language_dict(user):
     language_stats = []
     total_count = 0
 
+    # get languages for al user repos
     for repo in user_repos:
         for lang in repo.iter_languages():
+            #print(repo, lang)
             language_stats.append(lang)
             total_count += lang[1]
 
+    # create a language: bytes dictionary
     lang_dict = {}
     for lang in language_stats:
         if lang[0] in lang_dict:
@@ -31,13 +40,17 @@ def get_language_dict(user):
     return lang_dict, total_count
 
 
-
 def get_lang_percentages(user):
-    lang_dict, total_count = get_language_dict(user)
+    """
+    get language use percentages for a github user
+    :param user: user to stalk
+    :return: language: usage percentage, total bytecount of all languages
+    """
+    lang_dict, total_count = get_language_bytes(user)
     for key in lang_dict.keys():
         lang_dict[key] = lang_dict[key] / total_count * 100
 
-    return lang_dict
+    return lang_dict, total_count
 
 if __name__ == '__main__':
     print(get_lang_percentages('tpei'))
