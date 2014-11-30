310d667b88eac59662d7be7375fda548c94bbed0
Thomas Peikert <t.s.peikert@gmail.com>
2014-11-30 08:55:14 +0100

    private repositories are now included in the stats

--- lang_dict.py
+++ lang_dict.py
@@ -18,7 +18,9 @@ def get_language_bytes(user):
     user = g.user(user)
 
     # put all user repos in list
-    user_repos = [f for f in g.iter_user_repos(user.login)]
+    # iter_user_repos without private repos
+    # iter_repos includes private repos
+    user_repos = [f for f in g.iter_repos(user.login)]
     language_stats = []
     total_count = 0
 
@@ -53,4 +55,4 @@ def get_lang_percentages(user):
     return lang_dict, total_count
 
 if __name__ == '__main__':
-    print(get_repo_bytes('tpei'))
+    print(get_lang_percentages('tpei'))
--- repo_dict.py
+++ repo_dict.py
@@ -18,7 +18,7 @@ def get_repo_bytes(user):
     user = g.user(user)
 
     # put all user repos in list
-    user_repos = [f for f in g.iter_user_repos(user.login)]
+    user_repos = [f for f in g.iter_repos(user.login)]
     language_stats = []
     total_count = 0
 
